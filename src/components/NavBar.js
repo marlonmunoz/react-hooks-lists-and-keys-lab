e784ab0a0bae4e614873a9e3785bc9a263f877fe
Marlon Munoz <marlon@marlonshade.com>
2024-08-06 23:03:49 -0400

    list and Keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const linkELements = links.map((link, index) => {
+    // console.log(link);
+    return (
+      <a key={index} href={'#' + link}>{link}</a>
+    )
+  })
+  return <nav>{linkELements}</nav>
 }
 
 export default NavBar;
